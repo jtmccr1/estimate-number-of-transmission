7de9a43794a062973895eb3f8136fada44ec8501
JT <mccrone@umich.edu>
2020-01-31 10:17:58 +0000

    Updates

---
+++ precache-manifest.576dfca0f8813b93f9b74459f61d6ae3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2919e7b0a40187ddeb1aefc4e74f961a",
+    "url": "/estimate-number-of-transmission/index.html"
+  },
+  {
+    "revision": "db9384fa8313cec643e1",
+    "url": "/estimate-number-of-transmission/static/css/main.4209cd48.chunk.css"
+  },
+  {
+    "revision": "ef622878336ef083c73f",
+    "url": "/estimate-number-of-transmission/static/js/2.90e71397.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/estimate-number-of-transmission/static/js/2.90e71397.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db9384fa8313cec643e1",
+    "url": "/estimate-number-of-transmission/static/js/main.2bf4d7a9.chunk.js"
+  },
+  {
+    "revision": "0524edb4a319e8bb3b69",
+    "url": "/estimate-number-of-transmission/static/js/runtime-main.d9020be5.js"
+  }
+]);
